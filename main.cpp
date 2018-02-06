1abdba85b02a76b9f7e597b2fb272bbbfa50f443
wandrisco <wandrisco@users.noreply.github.com>
2018-02-05 19:00:02 -0600

    v1.5
    cleaned the code up a bit and added a feature or two. run it and make sure everything works. k thanks

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 #include <math.h>
-#include <stdlib.h> //included for rand
-#include <time.h> //included for time
 #include <fstream>
-#include <ctime>
+#include <iomanip>
 
 using namespace std;
 
 void menu(), returnMenu(), choice(int input);
-
-
-
 
 
 void Fibonacci() {
@@ -79,7 +74,6 @@ void originalEuclid100(int a[100], int b[100], int length) {
     ofstream myFile ("100Euclid.csv");
     if (myFile.is_open()){                      //if the file to save the results in will open
         for (int i = 0; i <= length; i++) {     //i starts at 0 and goes until the length of what is passed in
-            //start = clock();
             do {
                 if ((a[i] || b[i]) >= 1){
                     start1 = clock();
@@ -131,7 +125,6 @@ void improvedEuclid() {
     cin >> a;
     cout << "Please input a integer value for b: ";
     cin >> b;
-
     if ((a >= b) && (b > 0)) {
         do {
             remainder = a - b;
@@ -155,15 +148,14 @@ void improvedEuclid() {
 }
 
 void improvedEuclid100(int a[100], int b[100], int length){
-
     int remainder;
     double msecs1, msecs2;
-    clock_t start1, end1, start2, end2;
+    clock_t start1, end1, start2, end2, start3, end3, msecs3;
     ofstream myFile ("100improvedEuclid.csv");
 
     for (int i = 0; i <= length; i++) {
-        //start = clock();
         do {
+
             if ((a[i] >= b[i]) && (b[i] > 0)) {
                 start1 = clock();
                 cout << i << ". " << endl;
@@ -202,7 +194,7 @@ void improvedEuclid100(int a[100], int b[100], int length){
                 myFile << "The current value for a: " << a[i] << endl;
                 cout << "The current value for b: " << b[i] << endl; // cout the current randomly generated value for b
                 myFile << "The current value for b: " << b[i] << endl;
-                do { // Iproved Euclid algorithm with a and b switched from previously
+                do { // Improved Euclid algorithm with a and b switched from previously
                     remainder = b[i] - a[i];
                     if (remainder > a[i]) {
                         remainder = (remainder - a[i]);
@@ -223,23 +215,21 @@ void improvedEuclid100(int a[100], int b[100], int length){
                 cout << "The time it takes is: " << msecs2 << " milliseconds" << endl << endl;
                 myFile << "The time it takes is: " << msecs2 << " milliseconds" << endl << endl;
                 i++; // incrementing for the for loop
-			}
+            }
             else {
-                start2 = clock();
+                start3 = clock();
                 cout << i << ". " << endl;
                 myFile << i << ". " << endl;
                 cout << "The current value for a: " << a[i] << endl;
                 myFile << "The current value for a: " << a[i] << endl;
                 cout << "The current value for b: " << b[i] << endl;
                 myFile << "The current value for b: " << b[i] << endl;
-
-
                 cout << "The GCD of Euclid's Improved Algorithm is: 0" << endl;
                 myFile << "The GCD of Euclid's Improved Algorithm is: 0" << endl;
-                end2 = clock();
-                msecs2 = ((double) (end2 - start2)) * 1000 / CLOCKS_PER_SEC;
-                cout << "The time it takes is: " << msecs2 << " milliseconds" << endl << endl;
-                myFile << "The time it takes is: " << msecs2 << " milliseconds" << endl << endl;
+                end3 = clock();
+                msecs3 = ((double) (end3 - start3)) * 1000 / CLOCKS_PER_SEC;
+                cout << "The time it takes is: " << msecs3 << " milliseconds" << endl << endl;
+                myFile << "The time it takes is: " << msecs3 << " milliseconds" << endl << endl;
                 i++;
             }
         } while (i <=99);
@@ -248,45 +238,48 @@ void improvedEuclid100(int a[100], int b[100], int length){
     myFile.close();
 }
 
-/*
- * RANDOM100NUMBERS fills two arrays with random ints and passes into euclidoriginal100
- * for some reason, when passing the values, only passing the first elements of the arrays into euclidoriginal100
- */
-
-void random100Numbers(){
+void assignment1(){
     static int randomA[100], randomB[100], randomA_copy[100], randomB_copy[100], randomA_copy1[100], randomB_copy1[100];
-    static int a[100], b[100];
-    int i = 0;
+    char answer;
 
     for (int i = 0; i <= 99; ++i){
         randomA[i] = rand() % 100 + 0;
         randomB[i] = rand() % 100 + 0;
-        cout << "The random value for a" << i <<" is: " << randomA[i] << endl;
-        cout << "The random value for b" << i <<" is: " << randomB[i] << endl;
     }
-    cout << endl;
-    a[i] = randomA[i];
-    b[i] = randomB[i];
 
+    cout << "Would you like to view the random numbers? (y/n): " ;
+    cin >> answer;
+    cin.ignore();
+    cout << endl;
+    while (answer) {
+        switch (answer) {
+
+            case 'n':
+                break;
+
+            case 'y':
+
+                for (int i = 0; i <= 99; ++i){
+                    cout << "a" << i <<" is: " << randomA[i];
+                    cout << setw(10) << "b" << i <<" is: " << randomB[i];
+                    cout << endl;
+                }
+                break;
+
+            default:
+                break;
+        };
+        break;
+    }
     memcpy(&randomA_copy, &randomA, sizeof(randomA));
     memcpy(&randomB_copy, &randomB, sizeof(randomB));
     memcpy(&randomA_copy1, &randomA, sizeof(randomA));
     memcpy(&randomB_copy1, &randomB, sizeof(randomB));
-
     originalEuclid100(randomA_copy, randomB_copy, 100);
-
     cout << endl << "****IMPROVED****" << endl << endl;
-
     improvedEuclid100(randomA_copy1, randomB_copy1, 100);
-
 }
 
-
-
-/*
- * this is displayed when main is ran.
- * the CHOICE function coordinates with the menu function.
-*/
 void menu() {
     int input;
     cout << "*CSC 332*\n"
@@ -294,8 +287,7 @@ void menu() {
     cout << "1 - Fibonacci Exercise\n"
          << "2 - Euclid's Original Algorithm\n"
          << "3 - Euclid's Improved Algorithm\n"
-         << "4 - Euclid's Original 100 Times\n"
-         << "5 - Random Numbers\n"
+         << "4 - Programming Assignment 1\n"
          << "9 - Terminate Program\n\n";
     cout << "Enter a menu option: ";
     cin >> input;
@@ -303,10 +295,7 @@ void menu() {
     cout << endl;
     choice(input);
 }
-/*
- * RETURNMENU ask the user if they would like to return to the MENU function
- * if the user hits N, the function is repeated
- */
+
 void returnMenu() {
     char input;
     cout << "Return to Main Menu? (y/n)\n";
@@ -324,15 +313,12 @@ void returnMenu() {
             default:
                 cout << "Invalid Input. Returning to main menu.\n";
                 menu();
+                break;
         }
         break;
     }
 }
 
-/*
- * this coordinates with the MENU function
- * case 'x' means that is the key you enter to call that function
- */
 void choice(int input) {
     while (input) {
         switch (input) {
@@ -349,11 +335,10 @@ void choice(int input) {
                 returnMenu();
                 break;
             case 4:
-                //originalEuclid100();
+                assignment1();
                 returnMenu();
                 break;
             case 5:
-                random100Numbers();
                 returnMenu();
                 break;
             case 9:
